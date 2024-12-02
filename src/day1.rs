7482d32d9a0ba728351dedf0026a0bbce39b37d8
Chris Petkau <47264230+chrispetkau@users.noreply.github.com>
2024-12-01 20:08:12 -0800

    Day 1, part 2 solved.

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,7 @@
-#[aoc_generator(day1)]
-pub fn transform_input(input: &str) -> (Vec<usize>, Vec<usize>) {
+use std::collections::HashMap;
+
+#[aoc(day1, part1)]
+pub fn part1(input: &str) -> usize {
     let (mut a, mut b): (Vec<_>, Vec<_>) = input
         .lines()
         .map(|line| {
@@ -11,11 +13,6 @@ pub fn transform_input(input: &str) -> (Vec<usize>, Vec<usize>) {
         .unzip();
     a.sort();
     b.sort();
-    (a, b)
-}
-
-#[aoc(day1, part1)]
-pub fn part1((a, b): &(Vec<usize>, Vec<usize>)) -> usize {
     a.iter()
         .zip(b.iter())
         .map(|(a, b)| a.abs_diff(*b))
@@ -23,8 +20,19 @@ pub fn part1((a, b): &(Vec<usize>, Vec<usize>)) -> usize {
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &(Vec<usize>, Vec<usize>)) -> usize {
-    0
+pub fn part2(input: &str) -> usize {
+    let mut a = Vec::new();
+    let mut b = HashMap::new();
+    input.lines().for_each(|line| {
+        let mut elements = line
+            .split_ascii_whitespace()
+            .map(|element| element.parse::<usize>().unwrap());
+        a.push(elements.next().unwrap());
+        b.entry(elements.next().unwrap())
+            .and_modify(|count| *count += 1)
+            .or_insert(1);
+    });
+    a.iter().map(|a| a * b.get(a).unwrap_or(&0)).sum::<usize>()
 }
 
 #[cfg(test)]
@@ -38,13 +46,23 @@ mod tests {
 3   9
 3   3";
 
-    #[test]
-    fn part1() {
-        assert_eq!(super::part1(&super::transform_input(INPUT)), 11);
+    fn read_input() -> String {
         let mut input = String::new();
         let _ = std::fs::File::open("input\\2024\\day1.txt")
             .unwrap()
             .read_to_string(&mut input);
-        assert_eq!(super::part1(&super::transform_input(&input)), 1765812);
+        input
+    }
+
+    #[test]
+    fn part1() {
+        assert_eq!(super::part1(INPUT), 11);
+        assert_eq!(super::part1(&read_input()), 1765812);
+    }
+
+    #[test]
+    fn part2() {
+        assert_eq!(super::part2(INPUT), 31);
+        assert_eq!(super::part2(&read_input()), 20520794);
     }
 }
