5a3d4470cc15be205bd3e668b8b0b3d75afd434c
Chris Petkau <47264230+chrispetkau@users.noreply.github.com>
2024-12-03 16:40:19 -0800

    Day 3, part 2 solved

--- src/day3.rs
+++ src/day3.rs
@@ -3,8 +3,6 @@
 //     input
 // }
 
-use std::mem::MaybeUninit;
-
 use regex::Regex;
 
 #[aoc(day3, part1)]
@@ -20,14 +18,29 @@ pub fn part1(input: &str) -> usize {
 
 #[aoc(day3, part2)]
 pub fn part2(input: &str) -> usize {
-    0
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)").unwrap();
+    let mut sum = 0;
+    let mut enabled = true;
+    re.captures_iter(input).for_each(|instruction| {
+        let full = instruction.get(0).unwrap().as_str();
+        if full.starts_with("mul") {
+            if enabled {
+                let lhs = instruction.get(1).unwrap().as_str();
+                let rhs = instruction.get(2).unwrap().as_str();
+                sum += lhs.parse::<usize>().unwrap() * rhs.parse::<usize>().unwrap();
+            }
+        } else if full.starts_with("don") {
+            enabled = false;
+        } else {
+            enabled = true;
+        }
+    });
+    sum
 }
 
 #[cfg(test)]
 mod tests {
     use std::io::Read;
-
-    const INPUT: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
 
     fn read_input() -> String {
         let mut input = String::new();
@@ -39,13 +52,17 @@ mod tests {
 
     #[test]
     fn part1() {
+        const INPUT: &str =
+            "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         assert_eq!(super::part1(&INPUT), 161);
         assert_eq!(super::part1(&read_input()), 166357705);
     }
 
     #[test]
     fn part2() {
-        // assert_eq!(super::part2(&super::transform_input(INPUT)), 4);
-        // assert_eq!(super::part2(&super::transform_input(&read_input())), 426);
+        const INPUT: &str =
+            "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(super::part2(&INPUT), 48);
+        assert_eq!(super::part2(&read_input()), 88811886);
     }
 }
